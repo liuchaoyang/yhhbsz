fbce3d9738ff89fe5f7f17630e1c039cec12a4b4
liuchaoyang <875818589@qq.com>
2019-01-09 20:12:35 +0800

    短信

--- src/com/yzxt/tran/ApiTest.java
+++ src/com/yzxt/tran/ApiTest.java
@@ -1,44 +1,28 @@
 package com.yzxt.tran;
 
-import java.net.URLEncoder;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
-
-import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.params.HttpMethodParams;
-
-import com.alibaba.fastjson.JSONObject;
 
 public class ApiTest {
-	private static String code = smsCode();
-
+//	private static String code = smsCode();
+//    private Logger logger = Logger.getLogger(ApiTest.class);
+	private static final String sms_url = "http://localhost:8086/sms/verifyCode";
 	// 普通短信
 	public String sendsms(String phone) throws Exception {
 		HttpClient httpClient = new HttpClient();
-		PostMethod postMethod = new PostMethod("*******");
+		PostMethod postMethod = new PostMethod(sms_url);
 		postMethod.getParams().setContentCharset("UTF-8");
-		postMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());
+//		postMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());
 
-		String accesskey = "**********"; // 用户开发key
-		String accessSecret = "********"; // 用户开发秘钥
+		postMethod.addParameter("mobile", phone);
+		String code = smsCode();
+		postMethod.addParameter("verify_code", code);
 
-		NameValuePair[] data = { new NameValuePair("accesskey", accesskey), new NameValuePair("secret", accessSecret),
-				new NameValuePair("sign", "8188"), new NameValuePair("templateId", "10359"),
-				new NameValuePair("mobile", phone),
-				new NameValuePair("content", URLEncoder.encode(ApiTest.code, "utf-8"))// （示例模板：{1}您好，您的订单于{2}已通过{3}发货，运单号{4}）
-		};
-		postMethod.setRequestBody(data);
 		postMethod.setRequestHeader("Connection", "close");
 
 		int statusCode = httpClient.executeMethod(postMethod);
-		System.out.println("statusCode: " + statusCode + ", body: " + postMethod.getResponseBodyAsString());
-		System.out.println(ApiTest.code+"=============");
-		return ApiTest.code;
+//		logger.info("ApiTest sendsms response:" + code);
+		return code;
 	}
 
 	/*
@@ -46,7 +30,7 @@ public class ApiTest {
 	 * ApiTest(); //普通短信 t.sendsms(); }
 	 */
 	// 创建验证码
-	public static String smsCode(){
+	private String smsCode(){
         String random=(int)((Math.random()*9+1)*100000)+""; 
         System.out.println("验证码："+random);
         return random;
@@ -67,4 +51,12 @@ public class ApiTest {
 		}
 	}*/
 
+	public static void main(String[] args) {
+		ApiTest t = new ApiTest();
+		try {
+			t.sendsms("13691156267");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
